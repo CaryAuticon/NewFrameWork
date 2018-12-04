8389223ed91b1089fba21207e104ec95bed5c4b4
Cary <Cary.Son@auticon.us>
2018-12-04 15:56:55 -0800

    Removed needless text from JavaToJSON_ConvertTest class

---
+++ src/test/java/org/brit/tests/JavaToJSON_ConvertTest.java
@@ -0,0 +1,31 @@
+package org.brit.tests;
+
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class JavaToJSON_ConvertTest extends JavaToJSON_Convert {
+
+
+
+    @Test
+    public void SetName() {
+    }
+
+    @Test
+    public void SetId() {
+    }
+
+    @Test
+    public void SetPosition() {
+    }
+
+    @Test
+    public void SetExperience() {
+    }
+
+    @Test
+    public void SetSkills() {
+    }
+}
