04e11ba6964886919e2a7d0a275c9dbcaa6f81b9
Cary <Cary.Son@auticon.us>
2018-12-05 13:13:07 -0800

    Added comments to JavaToJSON_ConvertTest class. Comments describe what I'm attempting to do - call from javaToJSON_Convert.java class

--- src/test/java/org/brit/tests/JavaToJSON_ConvertTest.java
+++ src/test/java/org/brit/tests/JavaToJSON_ConvertTest.java
@@ -9,30 +9,42 @@ import static org.testng.Assert.*;
 public class JavaToJSON_ConvertTest extends JavaToJSON_Convert {
 
 
-
     @Test
     public void SetName() {
         createDummyObject();
 
-    }
+        // Attempting to call the code from JavaToJSON_Convert.java code: public static Resources createDummyObject() {    }
 
-    @Test
-    public void SetId() {
-        createDummyObject();
-    }
+        @Test
+        public void SetId () {
+            createDummyObject();
 
-    @Test
-    public void SetPosition() {
-        createDummyObject();
-    }
+            // Attempting to call the code from JavaToJSON_Convert.java code: public static Resources createDummyObject() {    }
 
-    @Test
-    public void SetExperience() {
-        createDummyObject();
-    }
+        }
 
-    @Test
-    public void SetSkills() {
-        createDummyObject();
+        @Test
+        public void SetPosition () {
+            createDummyObject();
+
+            // Attempting to call the code from JavaToJSON_Convert.java code: public static Resources createDummyObject() {    }
+
+        }
+
+        @Test
+        public void SetExperience () {
+            createDummyObject();
+
+            // Attempting to call the code from JavaToJSON_Convert.java code: public static Resources createDummyObject() {    }
+
+        }
+
+        @Test
+        public void SetSkills () {
+            createDummyObject();
+
+            // Attempting to call the code from JavaToJSON_Convert.java code: public static Resources createDummyObject() {    }
+
+        }
     }
 }
